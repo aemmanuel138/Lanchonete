64604e09f65e07a23ebb44658e64d3316b61b107
Djair Alves <>
2021-05-30 02:01:06 +0000

    Updtae de Teste

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Lanchonete 1.0"); //hehehe
-	 	Metas.showMetas(2); // Agora escolha o número 1
+	 	Metas.showMetas(9); // Agora escolha o número 1
 		Cliente c1  = new Cliente("Testando", 111,"+55 xx 9xx xxx xxx", 12.5);
 		System.out.println(c1);
   }
 }
- //Mãos a obra!
-//Yes, poderiámos criar um arquivo com os requisitos e irmos batendo as metas. Já comecei lá o arquivo de metas
 
 //Então vamos iniciar!
 
